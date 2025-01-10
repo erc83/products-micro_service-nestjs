ce46887491bb62749e07321a28512f131eaa2d7b
erc83 <espinoza.eric@gmail.com>
2025-01-10 02:55:42 -0300

    add: validation dto products

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,              // valida en conjunto con class-validator
+      forbidNonWhitelisted: true,   // properties tienen que venir como yo digo
+    })
+  )
+
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
